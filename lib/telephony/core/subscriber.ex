709cafaea8467ddd13089ef22c99eacf0e571708
AlexCsms <cosmasalex365@gmail.com>
2023-11-26 21:20:21 +0300

    Subscriber making a call and recharge

--- lib/telephony/core/subscriber.ex
+++ lib/telephony/core/subscriber.ex
@@ -12,6 +12,25 @@ defmodule Telephony.Core.Subscriber do
     payload = %{payload | subscriber_type: %Postpaid{}}
     struct(__MODULE__, payload)
   end
+
+  def make_call(%{subscriber_type: subscriber_type} = subscriber, time_spent, date)
+      when subscriber_type.__struct__ == Postpaid do
+    Postpaid.make_call(subscriber, time_spent, date)
+  end
+
+  def make_call(%{subscriber_type: subscriber_type} = subscriber, time_spent, date)
+      when subscriber_type.__struct__ == Prepaid do
+    Prepaid.make_call(subscriber, time_spent, date)
+  end
+
+  def make_recharge(%{subscriber_type: subscriber_type} = subscriber, value, date)
+      when subscriber_type.__struct__ == Prepaid do
+    Prepaid.make_recharge(subscriber, value, date)
+  end
+
+  def make_recharge(_, _, _) do
+    {:error, "Only prepaid can make a recharge"}
+  end
 end
 
 # payload
--- test/telephony/core/subscriber_test.exs
+++ test/telephony/core/subscriber_test.exs
@@ -1,7 +1,23 @@
 defmodule Telephony.Core.SubscriberTest do
   use ExUnit.Case
   # doctest Subscriber.Subscriber
-  alias Telephony.Core.{Postpaid, Prepaid, Subscriber}
+  alias Telephony.Core.{Call, Postpaid, Prepaid, Recharge, Subscriber}
+
+  setup do
+    postpaid = %Subscriber{
+      full_name: "Alex",
+      phone_number: "123",
+      subscriber_type: %Postpaid{spent: 0}
+    }
+
+    prepaid = %Subscriber{
+      full_name: "Alex",
+      phone_number: "123",
+      subscriber_type: %Prepaid{credits: 10, recharges: []}
+    }
+
+    %{postpaid: postpaid, prepaid: prepaid}
+  end
 
   test "create a prepaid subscriber" do
     # Given
@@ -15,34 +31,88 @@ defmodule Telephony.Core.SubscriberTest do
     result = Subscriber.new(payload)
 
     # Then
-    expect = %Subscriber{
+    expected = %Subscriber{
       full_name: "Alex",
       phone_number: "123",
       subscriber_type: %Prepaid{credits: 0, recharges: []}
     }
 
-    assert expect == result
+    assert expected == result
   end
 
-  test "create a postpaid subscriber" do
+  test "make a postpaid call", %{postpaid: postpaid} do
     # Given
-    payload = %{
+    date = NaiveDateTime.utc_now()
+    result = Subscriber.make_call(postpaid, 2, date)
+
+    expected = %Subscriber{
       full_name: "Alex",
-      phone_number: "2345",
-      subscriber_type: :postpaid
+      phone_number: "123",
+      subscriber_type: %Postpaid{spent: 2.08},
+      calls: [
+        %Call{
+          time_spent: 2,
+          date: date
+        }
+      ]
     }
 
-    # When
-    result = Subscriber.new(payload)
+    assert expected == result
 
     # Then
-    expect = %Subscriber{
+  end
+
+  test "make a prepaid call", %{prepaid: prepaid} do
+    # Given
+    date = NaiveDateTime.utc_now()
+    result = Subscriber.make_call(prepaid, 2, date)
+
+    expected = %Subscriber{
+      calls: [
+        %Call{
+          date: date,
+          time_spent: 2
+        }
+      ],
       full_name: "Alex",
-      phone_number: "2345",
-      subscriber_type: %Postpaid{spent: 0}
+      phone_number: "123",
+      subscriber_type: %Prepaid{
+        credits: 7.1,
+        recharges: []
+      }
     }
 
-    assert expect == result
+    assert expected == result
+
+    # Then
+  end
+
+  test "make a recharge", %{prepaid: prepaid} do
+    # Given
+    date = NaiveDateTime.utc_now()
+    result = Subscriber.make_recharge(prepaid, 2, date)
+
+    expected = %Subscriber{
+      calls: [],
+      full_name: "Alex",
+      phone_number: "123",
+      subscriber_type: %Prepaid{
+        credits: 12,
+        recharges: [
+          %Recharge{value: 2, date: date}
+        ]
+      }
+    }
+
+    assert expected == result
+  end
+
+  test "throw error when is not a prepaid", %{postpaid: postpaid} do
+    # Given
+    date = NaiveDateTime.utc_now()
+    result = Subscriber.make_recharge(postpaid, 2, date)
+    expected = {:error, "Only prepaid can make a recharge"}
+    assert expected == result
   end
 end
 
